ae91df7fbd1debdd778ccc549b6d8f91aad91d7c
Maxi <maxmilianleunig@gmail.com>
2023-04-26 15:16:02 +0200

    searchbar hinzugefügt

--- app/routes/gästebuch.tsx
+++ app/routes/gästebuch.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Button from "~/components/button";
 import supabase from "utils/supabase";
-import { useLoaderData } from "@remix-run/react";
-import { ActionArgs } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { ActionArgs, LoaderArgs } from "@remix-run/node";
+import data from "~/components/data";
 
-export const loader = async () => {
+export const loader = async ({ request }: LoaderArgs) => {
+  /* const { data } = await supabase.from("gästebuch").select(); */
+  const url = new URL(request.url);
+  const search = url.searchParams.get("search");
+
+  if (search) {
+    const { data, error } = await supabase
+      .from("gästebuch")
+      .select()
+      .ilike("name", `%${search}%`);
+
+    return { data };
+  }
   const { data } = await supabase.from("gästebuch").select();
   return { data };
 };
@@ -25,58 +38,71 @@ export async function action({ request }: ActionArgs) {
 export default function gästebuch() {
   const { data } = useLoaderData();
   return (
-    <form method="post">
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <div>
-        <h1 className="text-3xl">Gästebuch</h1>
+    <div>
+      <Form method="get">
+        <input
+          className="border-2 border-black rounded-lg text-black gap-2"
+          type="text"
+          name="search"
+          placeholder="search"
+        ></input>
+        <button>suchen</button>
+      </Form>
+
+      <Form method="post">
         <br />
-        <div className="flex flex-col w-[200px]">
-          Name:
-          <input
-            className="border-2 border-black rounded-lg text-black"
-            type="text"
-            name="name"
-            placeholder="name"
-          ></input>
-        </div>
-        <br />
-        Datum der Ankunft:
-        <div className="w-[200px]">
-          <input
-            className="border-2 border-black rounded-lg text-black"
-            type="text"
-            name="ankunfts_datum"
-            placeholder="ankunfts_datum"
-          ></input>
+
+        <div>
+          <h1 className="text-3xl">Gästebuch</h1>
           <br />
-          <br />
-          Kurznachricht (Optional):
-          <div className="">
-            <textarea
-              className="border-2 border-black rounded-lg text-black w-[300px] p-2"
-              cols={6}
-              name="kurznachricht"
-              placeholder="kurznachricht"
-            ></textarea>
-            <br />
-            <br />
-            <div className="link">
-              <button>Speichern</button>
-            </div>
+          <div className="flex flex-col w-[200px]">
+            Name:
+            <input
+              className="border-2 border-black rounded-lg text-black"
+              type="text"
+              name="name"
+              placeholder="name"
+            ></input>
           </div>
-          {data.map((entry: any, index: any) => (
-            <div key={index}>
-              <div className="flex flex-col">
-                {entry.name}
-                <br />
-                {entry.ankunfts_datum}
-                <br />
-                {entry.kurznachricht}
+          <br />
+          Datum der Ankunft:
+          <div className="w-[200px]">
+            <input
+              className="border-2 border-black rounded-lg text-black"
+              type="text"
+              name="ankunfts_datum"
+              placeholder="ankunfts_datum"
+            ></input>
+            <br />
+            <br />
+            Kurznachricht (Optional):
+            <div className="">
+              <textarea
+                className="border-2 border-black rounded-lg text-black w-[300px] p-2"
+                cols={6}
+                name="kurznachricht"
+                placeholder="kurznachricht"
+              ></textarea>
+              <br />
+              <br />
+              <div className="link">
+                <button>Speichern</button>
               </div>
             </div>
-          ))}
+            {data.map((entry: any, index: any) => (
+              <div key={index}>
+                <div className="flex flex-col">
+                  {entry.name}
+                  <br />
+                  {entry.ankunfts_datum}
+                  <br />
+                  {entry.kurznachricht}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </form>
+      </Form>
+    </div>
   );
 }
